3798e7a1c5ac68103afdfa8060c7c43c2fd8dcac
Qi Yang <qi.yang@accuity.com>
2019-02-20 11:57:19 -0600

    Sync

--- src/main/java/com/bookstore/PersistenceConfig.java
+++ src/main/java/com/bookstore/PersistenceConfig.java
@@ -1,23 +1,81 @@
-//package com.bookstore;
-//
-//import javax.sql.DataSource;
-//
-//import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-//import org.springframework.boot.context.properties.ConfigurationProperties;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//
-//@Configuration
-//public class PersistenceConfig {
+package com.bookstore;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.zaxxer.hikari.HikariDataSource;
+
+@Configuration
+@ConfigurationProperties(prefix = "spring.datasource")
+public class PersistenceConfig {
+	static Logger log = LoggerFactory.getLogger(PersistenceConfig.class);
+
+	private String url;
+	private String username;
+	private String password;
+	private String schema;
+	private String driverClassName;
+
+	@Bean
+	public DataSource dataSource(DataSourceProperties dataSourceProperties) {
+		log.info("SSH connect started by DataSourceConfig");
+		HikariDataSource dataSource = DataSourceBuilder.create().type(HikariDataSource.class)
+				.username(dataSourceProperties.getUsername()).password(dataSourceProperties.getPassword())
+				.url(dataSourceProperties.getUrl()).driverClassName(dataSourceProperties.getDriverClassName()).build();
+		return dataSource;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSchema() {
+		return schema;
+	}
+
+	public void setSchema(String schema) {
+		this.schema = schema;
+	}
+
+	public String getDriverClassName() {
+		return driverClassName;
+	}
+
+	public void setDriverClassName(String driverClassName) {
+		this.driverClassName = driverClassName;
+	}
+
 //	@Bean
-//	@ConfigurationProperties(prefix = "spring.datasource")
-//	public DataSource dataSource() {
+//	@ConfigurationProperties(prefix = "datasource.flyway")
+//	public DataSource inMemoryDataSource() {
 //		return DataSourceBuilder.create().build();
 //	}
-//	
-////	@Bean
-////	@ConfigurationProperties(prefix = "datasource.flyway")
-////	public DataSource inMemoryDataSource() {
-////		return DataSourceBuilder.create().build();
-////	}
-//}
+
+}
